17737ea58a1d12d4c524c4b081035ed85043bfce
squeaky1273 <pjriddell22@gmail.com>
2020-11-24 00:48:12 -0800

    Initial new channel

--- sockets/chat.js
+++ sockets/chat.js
@@ -20,12 +20,16 @@ module.exports = (io, socket, onlineUsers) => {
     socket.emit('get online users', onlineUsers);
   })
 
-// This fires when a user closes out of the application
-// socket.on("disconnect") is a special listener that fires when a user exits out of the application.
-socket.on('disconnect', () => {
-  //This deletes the user by using the username we saved to the socket
-  delete onlineUsers[socket.username]
-  io.emit('user has left', onlineUsers);
-});
+  // This fires when a user closes out of the application
+  // socket.on("disconnect") is a special listener that fires when a user exits out of the application.
+  socket.on('disconnect', () => {
+    //This deletes the user by using the username we saved to the socket
+    delete onlineUsers[socket.username]
+    io.emit('user has left', onlineUsers);
+  });
+
+  socket.on('new channel', (newChannel) => {
+    console.log(newChannel);
+  })
 
 }
